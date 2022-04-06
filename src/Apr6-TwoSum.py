21437aa683a1f5729ac894e3821c0e13ef2fa2f6
jace.riehl <jace.riehl@coldboretechnology.com>
2022-04-06 11:00:46 -0600

    Inputting beginning LeetCode files
    - Adding TwoSum from Leetcode.
    - Adding README

---
+++ src/Apr6-TwoSum.py
@@ -0,0 +1,31 @@
+# Solution 1 : 2 pointers - not a great solution because the array doesnt come in sorted
+# def twoSum(nums, target):
+#     nums2 = nums.copy()
+#     nums2.sort()
+#     l = 0
+#     r = len(nums2) -1
+#     while l < r: 
+#         if nums2[l] + nums2[r] < target:
+#             l += 1
+#         elif nums2[l] + nums2[r] > target:
+#             r -= 1
+#         else:
+#             if nums2[l] == nums2[r]: 
+#                 ar = [nums.index(nums2[l]), nums.index(nums2[r], l+1, len(nums2))]
+#             else: ar = [nums.index(nums2[l]), nums.index(nums2[r])]
+#             ar.sort()
+#             return ar
+            
+# Solution 2 : using a dictionary
+def twoSum(nums, target):
+    dict = {}
+    for i, num in enumerate(nums):
+        if num in dict:
+            return [dict[num],i]
+        else:
+            dict[target - num] = i
+    return []
+            
+arr = twoSum([5,75,25], 100)
+
+print(arr)
