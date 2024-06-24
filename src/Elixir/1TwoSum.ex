871dd7e9d8582f44b2661ca227e8ec63502c68fd
jace.riehl <jace.riehl@gmail.com>
2024-06-23 21:14:47 -0600

    Using Enum instead of recursion

--- src/Elixir/1TwoSum.ex
+++ src/Elixir/1TwoSum.ex
@@ -1,3 +1,4 @@
+# tail recursion
 defmodule Solution do
   @spec two_sum(nums :: [integer], target :: integer) :: [integer]
   def two_sum([num | tail], target, offset \\ 1) do
@@ -11,4 +12,21 @@ defmodule Solution do
         two_sum(tail, target, offset + 1)
     end
   end
+end
+
+# using Enum
+defmodule Solution2 do
+  @spec two_sum(nums :: [integer], target :: integer) :: [integer]
+  def two_sum(nums, target) do
+    IO.inspect(nums)
+    nums
+        |> Enum.with_index()
+        |> Enum.reduce_while(%{}, fn {num, index}, map ->
+            IO.inspect map
+            case map[target - num] do
+                nil -> {:cont, Map.put(map,num,index)}
+                found -> {:halt, [index, found]}
+            end
+        end)
+    end
 end
