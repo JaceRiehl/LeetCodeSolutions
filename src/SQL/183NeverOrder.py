449baa023f5b68a45ad52dcc075d3cae1249e03a
jace.riehl <jace.riehl@gmail.com>
2024-08-13 16:04:40 -0600

    more efficent 183 Pandas solution

--- src/SQL/183NeverOrder.py
+++ src/SQL/183NeverOrder.py
@@ -5,3 +5,8 @@ def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFram
     naCustomers = df['customerId'].isna()
     res = df[naCustomers]
     return res[['name']].rename(columns={'name': 'Customers'})
+
+def find_customers2(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    mask = -customers['id'].isin(orders['customerId'])
+    df = customers[mask]
+    return df[['name']].rename(columns={'name': 'Customers'})
